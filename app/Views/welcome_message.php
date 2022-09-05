3a7331ef28cbd7124d60df86117ffd46de12c161
Rio Agung Purnomo <91432414+rioagungpurnomo@users.noreply.github.com>
2022-09-05 10:04:19 +0700

    Update welcome_message.php

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -6,28 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to CodeSurfing</title>
-    <link rel="shortcut icon" href="https://codesurfing.herokuapp.com/assets/images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="https://codesurfing.my.id/assets/images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link href="https://codesurfing.herokuapp.com/assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://codesurfing.herokuapp.com/assets/css/codesurfing.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://codesurfing.my.id/assets/css/codesurfing.css" rel="stylesheet">
 </head>
 
 <body>
 
     <nav class="navbar navbar-expand-md navbar-light bg-white sticky-top border-bottom">
         <div class="container">
-            <a class="navbar-brand mb-0" href="#"><img src="https://codesurfing.herokuapp.com/assets/images/codesurfing.png" class="logo me-1" alt="Codesurfing"><span class="text-codeigniter">Code</span><span class="text-codesurfing fw-semibold">Surfing</span></a>
+            <a class="navbar-brand mb-0" href="#"><img src="https://codesurfing.my.id/assets/images/codesurfing.png" class="logo me-1" alt="Codesurfing"><span class="text-codeigniter">Code</span><span class="text-codesurfing fw-semibold">Surfing</span></a>
             <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item"><a class="nav-link" target="_blank" href="https://codesurfing.herokuapp.com/news">News</a></li>
-                    <li class="nav-item"><a class="nav-link" target="_blank" href="https://codesurfing.herokuapp.com/discuss">Discuss</a></li>
-                    <li class="nav-item"><a class="nav-link" target="_blank" href="https://codesurfing.herokuapp.com/contribute">Contribute</a></li>
+                    <li class="nav-item"><a class="nav-link" target="_blank" href="https://codesurfing.my.id/news">News</a></li>
+                    <li class="nav-item"><a class="nav-link" target="_blank" href="https://codesurfing.my.id/discuss">Discuss</a></li>
+                    <li class="nav-item"><a class="nav-link" target="_blank" href="https://codesurfing.my.id/contribute">Contribute</a></li>
                 </ul>
                 <div class="d-flex" role="search">
-                    <a target="_blank" href="https://codesurfing.herokuapp.com/docs" class="btn btn-codesurfing me-2 btn-md" type="button">Documentation</a>
+                    <a target="_blank" href="https://codesurfing.my.id/docs" class="btn btn-codesurfing me-2 btn-md" type="button">Documentation</a>
                 </div>
             </div>
         </div>
@@ -74,7 +74,7 @@
                 </div>
                 <div class="flex-grow-1 ms-3">
                     <h5>Discuss</h5>
-                    <p class="fs-6">CodeSurfing is a community-developed open source project, with several vanues for the community members to gather and exchange ideas. View all the threads <a target="_blank" href="https://codesurfing.herokuapp.com/forum" class="text-decoration-none text-codesurfing">CodeSurfing's forum</a> !</p>
+                    <p class="fs-6">CodeSurfing is a community-developed open source project, with several vanues for the community members to gather and exchange ideas. View all the threads <a target="_blank" href="https://codesurfing.my.id/forum" class="text-decoration-none text-codesurfing">CodeSurfing's forum</a> !</p>
                 </div>
             </div>
         </div>
@@ -96,7 +96,7 @@
         </div>
     </footer>
 
-    <script src="https://codesurfing.herokuapp.com/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 
 </html>
